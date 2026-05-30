ee709a42738d26b8ab24f106f8f79f7066553f83
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2026-05-30 14:17:00 +0330

    Implementing a login page and handling the form to get the user number and send the mobile number to the backend

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import CheckOtpForm from "../components/templates/CheckOtpForm";
+import SendOtpForm from "../components/templates/sendOtpForm";
+
+function AuthPage() {
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+  return (
+    <div>
+      {step === 1 && <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />}
+      {step === 2 && <CheckOtpForm />}
+    </div>
+  );
+}
+
+export default AuthPage;
