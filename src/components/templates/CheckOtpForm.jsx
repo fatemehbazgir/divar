032c853c7ab15c09b7802447bc38304daa18bb40
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2026-05-30 22:54:24 +0330

    Getting the OTP code and confirming it

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,7 +1,34 @@
-function CheckOtpForm() {
+import { checkOtp } from "../../services/auth";
+
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    if (code.length !== 5) return;
+
+    const { response, error } = await checkOtp(mobile, code);
+
+    if (response) {
+      console.log(response);
+    }
+    if (error) console.log(error.response.data.message);
+  };
   return (
-    <div>CheckOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>تایید کد ارسال شده</p>
+      <span>کد ارسال شده به شماره "{mobile}" را وارد کنید.</span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 }
 
-export default CheckOtpForm
+export default CheckOtpForm;
